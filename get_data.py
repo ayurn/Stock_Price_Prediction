c7fa79b7dc403229d1f2c2f85f14510d5a1722b0
ayurn <ninaweayur@gmail.com>
2021-09-15 22:50:41 +0530

    [Ayur] Refactor : code for getting data from api.

--- get_data.py
+++ get_data.py
@@ -4,8 +4,7 @@ from decouple import config
 from alpha_vantage.timeseries import TimeSeries
 
 # Download stock data
-ticker = 'GOOGL' # take argument from console
-# api_key = open('alpha_key.txt').read()
+ticker = 'GOOGL'
 keys = config('KEYS')
 time = TimeSeries(key=keys, output_format='pandas')
 data, metadata = time.get_intraday(symbol=ticker, interval='1min', outputsize='full')
